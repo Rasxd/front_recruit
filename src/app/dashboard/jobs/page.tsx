a3cb73c6035547b2f3f614b0c49dc24f75ff7aca
Italo <italomonticeli@gmail.com>
2024-06-14 02:07:25 -0300

    feature: header improvements

---
+++ src/app/dashboard/jobs/page.tsx
@@ -0,0 +1,6 @@
+import styles from './page.module.css'
+export default function Jobs() {
+  return (
+    <h1 className={styles.pageTitle}>All Jobs</h1>
+  )
+} 
--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,35 +1,74 @@
+"use client"
+import { usePathname } from 'next/navigation'
+import { useRouter } from 'next/navigation'
 import { FaGear } from "react-icons/fa6";
+import { useState } from 'react';
 import styles from './layout.module.css'
-export default function DashboardLayout({
-  children, // will be a page or nested layout
-}: {
-  children: React.ReactNode
-}) {
+
+export default function DashboardLayout({ children }: { children: React.ReactNode }) {
+
+  const pathname = usePathname()
+
+  console.log('pagina atual', pathname)
+
+  const [isOpen, setIsOpen] = useState(false);
+
+  const router = useRouter()
+
+  const handleLogOut = () => {
+    router.push('/login')
+  }
+
+  const handleDolores = () => {
+    setIsOpen(!isOpen)
+  }
+
+  function checkSelectedPage(menu) {
+    return menu === pathname ? styles.pageSelected : ''
+  }
+
   return (
-    <section>
+    <section className={styles.dashbordContainer}>
       <div className={styles.headerWrapper}>
 
         <div className={styles.leftContent}>
           <h1>Greenhouse</h1>
+
           <div className={styles.buttonsWrapper}>
-            <button className={styles.headerButtons}>Platforms</button>
-            <button className={styles.headerButtons}>Why Greenhouse</button>
-            <button className={styles.headerButtons}>Resources</button>
-            <button className={styles.headerButtons}>About</button>
-            <button className={styles.headerButtons}>Community</button>
+            <button className={`${styles.headerButtons} ${checkSelectedPage('/dashboard/jobs')}`}>Jobs</button>
+            <button className={`${styles.headerButtons} ${checkSelectedPage('/dashboard/candidates')} `}>Candidates</button>
+            <button className={`${styles.headerButtons} ${checkSelectedPage('/dashboard/reports')} `}>Reports</button>
+            <button className={`${styles.headerButtons} ${checkSelectedPage('/dashboard/integrations')} `}>Integrations</button>
           </div>
         </div>
 
         <div className={styles.rightContent} >
+
           <FaGear />
-          <button className={styles.profileButton}>
-            LS
-          </button>
+          <div className={styles.profileWrapper}>
+            <button onClick={handleDolores} className={styles.profileButton}>
+              LS
+            </button>
+
+            {isOpen &&
+              <div className={styles.profileBox}>
+                <span className={styles.accountButtons}>Account settings</span>
+                <button onClick={handleLogOut} className={styles.accountButtons}>Log Out</button>
+              </div>
+            }
+          </div>
+
         </div>
 
       </div>
-      {children}
+      <div className={styles.childrenWrapper}>
+        {children}
+      </div>
+
+
+
     </section >
   )
 }
+
 
